ea5179178ecad139f38d8b18ef8222476fc0826e
giovannitamburini <giovatambu@gmail.com>
2023-09-14 14:58:14 +0200

    added adress class

---
+++ csharp-lista-indirizzi/Adress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Adress
+    {
+        // Street,City,Province,ZIP
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string Province { get; private set; }
+        public int Zip { get; private set; }
+
+        public Adress(string street, string city, string province, int zip)
+        {
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.Zip = zip;
+        }
+    }
+}
